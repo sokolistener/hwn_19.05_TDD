dffb3d5e2129c3cc542433ba37f9675d0efb43fc
sokolistener <holmse113@mail.ru>
2023-05-16 21:41:28 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,22 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.*;
+
 public class PhoneBookTest {
+    private PhoneBook phoneBook;
+
+    @BeforeEach
+    void initiation(){
+        phoneBook = new PhoneBook();
+    }
+    @Test
+    void testAdd() {
+        String name1 = "John";
+        String phone1 = "900889512";
+        String name2 = "Jack";
+        String phone2 = "900889513";
+
+        phoneBook.add(name1, phone1);
+        Assertions.assertEquals(2,phoneBook.add(name2,phone2));
+
+    }
 }
