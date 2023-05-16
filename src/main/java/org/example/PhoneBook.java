4e4364ce3fe3aeb1ab519f9613ef9116520c7150
sokolistener <holmse113@mail.ru>
2023-05-16 22:01:49 +0300

    Stub and tests for findByNumber.

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -12,4 +12,8 @@ public class PhoneBook {
         }
         return phoneBook.size();
     }
+
+    public String findByNumber (String number){
+        return null;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,5 +1,10 @@
 import org.example.PhoneBook;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class PhoneBookTest {
     private PhoneBook phoneBook;
@@ -17,6 +22,22 @@ public class PhoneBookTest {
 
         phoneBook.add(name1, phone1);
         Assertions.assertEquals(2,phoneBook.add(name2,phone2));
+    }
+    @ParameterizedTest
+    @MethodSource("sourceForFindByNumber")
+    void testFindByNumber(String searchingNumber, String expectedName) {
+        String name1 = "John";
+        String phone1 = "900889512";
+        String name2 = "Jack";
+        String phone2 = "900889513";
 
+        phoneBook.add(name1, phone1);
+        phoneBook.add(name2,phone2);
+        Assertions.assertEquals(expectedName,phoneBook.findByNumber(searchingNumber));
+    }
+
+    private static Stream<Arguments> sourceForFindByNumber() {
+        return Stream.of(Arguments.of("900889512", "John"),
+                Arguments.of("900889513", "Jack"));
     }
 }
