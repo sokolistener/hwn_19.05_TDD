48d8037c96473a236814ce52a403bb86a486f238
sokolistener <holmse113@mail.ru>
2023-05-17 20:25:14 +0300

    findByNumber implemented.

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,6 +17,10 @@ public class PhoneBook {
     }
 
     public String findByNumber (String number){
+        if (phoneBook.containsValue(number)){
+            BiMap<String, String> biMap = HashBiMap.create(phoneBook);
+            return biMap.inverse().get(number);
+        }
         return null;
     }
 }
